b79e476c16ffe4be553be5223ada33792b67fde9
developerKDK <akwdmfxu@naver.com>
2021-12-23 16:25:19 +0900

    [level_1] 숫자문자열과영단어 문제 풀이

--- [1주차]/2021 카카오 채용연계 인턴쉽/level1_숫자문자열과영단어/Solution_dkkang.java
+++ [1주차]/2021 카카오 채용연계 인턴쉽/level1_숫자문자열과영단어/Solution_dkkang.java
@@ -1,12 +1,31 @@
 public class Solution_dkkang {
 	public static void main(String[] args) {
-		System.out.println(solution(""));
+
+		//1478
+		String test1 = "one4seveneight";
+		//234567
+		String test2 = "23four5six7";
+		//234567
+		String test3 = "2three45sixseven";
+		String test4 = "1234";
+
+		Solution_dkkang s = new Solution_dkkang();
+		System.out.println(s.solution(test1));
 	}
 
-	static String[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+	String[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-	public static int solution(String s) {
-		return 0;
+	public int solution(String s) {
+
+		String answer = s;
+
+		for(int i=0; i<number.length; i++) {
+			if(answer.contains(number[i])) {
+				answer = answer.replaceAll(number[i], String.valueOf(i));
+			}
+		}
+
+		return Integer.valueOf(answer);
 	}
 
 }
