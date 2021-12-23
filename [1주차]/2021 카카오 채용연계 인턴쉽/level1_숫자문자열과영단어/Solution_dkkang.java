ae984eba7e32fcc136669cdd9b2f313a2336b5f6
developerKDK <akwdmfxu@naver.com>
2021-12-23 15:53:39 +0900

    테스트

---
+++ [1주차]/2021 카카오 채용연계 인턴쉽/level1_숫자문자열과영단어/Solution_dkkang.java
@@ -0,0 +1,12 @@
+public class Solution_dkkang {
+	public static void main(String[] args) {
+		System.out.println(solution(""));
+	}
+
+	static String[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+	public static int solution(String s) {
+		return 0;
+	}
+
+}
