3a95cfe45818148736ca781e982866711124faa0
sdh7700 <donghwa811@naver.com>
2022-01-21 19:19:12 +0900

    Create BOJ_1212_신동화.java

---
+++ [5주차]/BOJ_1212_신동화.java
@@ -0,0 +1,36 @@
+package BOJ_1212;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1212 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		String eight = br.readLine();
+		if(eight.equals("0"))
+			result.append(0);
+		else {
+			for(int i=0; i<eight.length(); i++) {
+				int sn = 4;
+				int target = eight.charAt(i) - '0';
+				while(sn >= 1) {
+					if(target - sn >= 0) {
+						result.append('1');
+						target -= sn;
+					}
+					else {
+						if(i != 0 || result.length() != 0)
+							result.append('0');					
+					}
+					sn /= 2;
+				}
+			}
+		}
+		
+		
+		System.out.println(result);
+	}
+}
